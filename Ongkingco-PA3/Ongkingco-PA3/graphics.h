cf6d75fa731aa47a039e134babc8ca84889a90b6
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-04-13 15:38:44 -0700

    PA3: Stub

---
+++ Ongkingco-PA3/Ongkingco-PA3/graphics.h
@@ -0,0 +1,41 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <iostream>
+using namespace std;
+
+#include "graphics_headers.h"
+#include "camera.h"
+#include "shader.h"
+#include "cube.h"
+
+class Graphics
+{
+  public:
+    Graphics(); // constructor
+    ~Graphics(); // destructor
+    bool Initialize(int width, int height); // set up camera, shader programs, and objects
+    void Update(unsigned int dt, glm::vec3 speed); // update/transform any objects
+    void Render(); // update view
+
+  private:
+    std::string ErrorString(GLenum error);
+    
+    Camera *m_camera;
+    Shader *m_shader;
+
+    // Locations of MVP matrices in shader
+    GLint m_projectionMatrix;
+    GLint m_viewMatrix;
+    GLint m_modelMatrix;
+
+    // Locations of vertex attributes in shader
+    GLint m_vertPos;
+    GLint m_vertCol;
+
+    // Interactable objects
+    Cube* m_triangle;
+
+};
+
+#endif /* GRAPHICS_H */
---
+++ Ongkingco-PA3/Ongkingco-PA3/pyramid.h
@@ -0,0 +1,42 @@
+#ifndef PYRAMID_H
+#define PYRAMID_H
+
+#include "graphics_headers.h"
+#include <vector>
+
+class Pyramid
+{
+    public:
+        Pyramid();
+        ~Pyramid();
+
+        void Initialize(GLint posAttribLoc, GLint colAttribLoc);
+        void Update(unsigned int dt);
+        void Render(GLint posAttribLoc, GLint colAttribLoc);
+
+        // Defines object vertices and triangle indexes
+        void createVertices();
+
+        glm::mat4 GetModel();
+
+        void setSpeed(glm::vec3 spd) { m_speed = spd; }
+        void setAngularSpeed(glm::vec3 spd) { m_angSpeed = spd; }
+
+    private:
+        // Define object in space
+        glm::mat4 model;
+        std::vector<Vertex> Vertices;
+        std::vector<unsigned int> Indices;
+
+        // Define vertex array and vertex/index buffer objects
+        GLuint vao;
+        GLuint VB;
+        GLuint IB;
+
+        // Define object speed and initial rotation angle
+        glm::vec3 m_speed = glm::vec3(0., 0., 0.);
+        glm::vec3 m_angSpeed = glm::vec3(0., 0., 0.);
+        float angle;
+};
+
+#endif
